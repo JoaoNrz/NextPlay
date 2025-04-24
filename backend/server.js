9fe1a31abaf2d260ded5740c9cff9e8f23477ec6
JoaoNrz <jpnarcizo02@gmail.com>
2025-04-23 22:09:02 -0300

    Estrutura Inicial Server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,30 @@
-console.log("Helo World")
+import express from 'express'
+import path from 'path'
+import dotenv from 'dotenv'
+import __dirname from './utils/pathUtils.js';
+
+
+//CARREGAR AS VARIAVEIS DO .ENV
+dotenv.config();
+
+
+const app = express();
+const port = process.env.PORT;
+
+app.listen(port,()=>{
+    console.log(`Servidor rodando na porta ${port}`);
+})
+
+app.get('/',(req,res)=>{
+    res.send(`
+        <html>
+            <head><title>NextPlay</title></head>
+            <body>
+                <h1>Hello World</h1>
+            </body>
+        </html>
+    `);
+})
+
+
+
