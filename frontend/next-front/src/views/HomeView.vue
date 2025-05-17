873641971e317af09c854cc3a4e90c66717d7940
JoaoNrz <jpnarcizo02@gmail.com>
2025-05-17 15:40:37 -0300

    Home Att

--- frontend/next-front/src/views/HomeView.vue
+++ frontend/next-front/src/views/HomeView.vue
@@ -4,9 +4,11 @@
     <div class="home-content">
       <NavBar :userInitial="'J'" class="navegation" />
       <div class="main-content">
-        <h1>Home</h1>
+        <h1>Novidades e tendências</h1>
         <p>Welcome to the home page!</p>
-        <p>This is where you can find the latest updates and news.</p>
+        <div class="extramsg">
+          <p>Sua nova plataforma completa para gestão de jogos</p>
+        </div>
       </div>
     </div>
 
@@ -35,13 +37,38 @@
 
     .home-content {
         display: flex;
-        align-items: center;
+        align-items: start;
         justify-content: center;
         padding: 20px 0px;
         width: 95%;
     }
     .main-content {
         width: 85%;
+    }
+
+    .main-content h1 {
+        font-size: 72px;
+        font-weight: bold;
+        color: #fff;
+        margin-bottom: 10px;
+    }
+
+    .extramsg{
+      width: 100%;
+      height: 80px;
+      background: linear-gradient(90deg, #ff39a6 0%, #ff8c3f 100%);
+      border-radius: 10px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin-top: 20px;
+    }
+
+    .extramsg p{
+      font-size: 26px;
+      font-weight: bold;
+      color: #fff;
+      letter-spacing: 0.2px;
     }
         
 </style>
